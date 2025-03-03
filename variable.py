05bb25dad8e190f64824dc1b2518fc58256d3f85
HASNAT <61647150+hasnatosman@users.noreply.github.com>
2025-03-03 13:47:56 +0600

    Variable
    variable & f -string

---
+++ variable.py
@@ -0,0 +1,35 @@
+# variable is like a bucket or container that can store information.
+
+# string: is a text
+
+first_name = "Hasnat"
+last_name = "Osman"
+
+full_name = first_name + last_name
+
+print(first_name)
+print(last_name)
+
+print(full_name)
+
+# f-string : a way of format string
+
+print(f"My name is {first_name} {last_name}!")
+
+# integer: is a whole nuber
+
+age = 25
+
+print(f"I am {age} years old!")
+
+# float: fractional number
+
+weight = 75.5
+
+print(f"My weight is {weight}!")
+
+# boolean
+
+is_student = True
+
+print(f"Are you a student ? {is_student}")
