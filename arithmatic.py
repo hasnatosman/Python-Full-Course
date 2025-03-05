ce82e49b8948fb4bfc465e3f42d25ce793eabe40
HASNAT <61647150+hasnatosman@users.noreply.github.com>
2025-03-05 23:48:58 +0600

    Arithmattic
    there are some arithmatic operators

---
+++ arithmatic.py
@@ -0,0 +1,30 @@
+friends = 10
+
+# friends = friends + 1  
+# friends += 1
+# friends = friends - 2
+# friends -= 2
+# friends = friends * 3
+# friends *= 3
+# friends = friends / 2
+# friends /= 2
+# friends = friends ** 2
+# friends **= 2
+# friends = friends % 3
+# friends %= 3
+
+print(friends)
+
+#math function
+
+x = 3.14
+y = 4
+z = 5
+
+
+# result = round(x)
+# result = abs(y)
+# result = pow(4,3)
+# result = max(x,y,z)
+result = min(x,y,z)
+print(result)
